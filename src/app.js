0ce62e9ba629b960d13d50116d9dda6f81d8918a
Phi Nguyen <pnguy55@gmail.com>
2019-11-11 12:03:56 -0600

    add more tests and index.html

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.use(userRouter)
