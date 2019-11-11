3250b8c9a32e4c874e4ac002d97977c11c335b06
Phi Nguyen <pnguy55@gmail.com>
2019-11-11 11:45:41 -0600

    add more tests and index.html

--- src/index.js
+++ src/index.js
@@ -51,6 +51,9 @@ const port = process.env.PORT
 // // Must register the router for use
 // app.use(userRouter)
 // app.use(taskRouter)
+app.get('', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'))
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
